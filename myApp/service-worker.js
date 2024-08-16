975d1a315f027dd3057ef8b0ba340da17651ca43
Qihan Wang <henrybalek@gmail.com>
2024-08-16 15:25:38 -0400

    feat:connect to firebase

--- myApp/service-worker.js
+++ myApp/service-worker.js
@@ -77,6 +77,6 @@ const addToFirebase = () => {
   const request=objectStore.getAll()
   request.onsuccess=(event)=>{
     console.log(event)
-    // firebaseWrite()
+    firebaseWrite(event)
   }
 };
